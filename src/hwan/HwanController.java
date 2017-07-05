7dfdb3f267022bab9add37560725a0ce28496dff
skies201018@gmail.com <skies2000@naver.com>
2017-07-05 15:20:05 +0900

    hwan컨트롤러 realpath로 변경

--- src/hwan/HwanController.java
+++ src/hwan/HwanController.java
@@ -96,12 +96,15 @@ public class HwanController {
 	
 	
 	
-	@SuppressWarnings("finally")
+	@SuppressWarnings({ "finally", "deprecation" })
 	public MultipartRequest getMul(HttpServletRequest req){
 		MultipartRequest mul= null;
 		
+		String uploadPath = req.getRealPath("images/");
+		
+		
 		try{
-			mul = new MultipartRequest(req,"C:\\workspace\\UnknownBike\\WebContent\\images", 1024*10000,"utf-8",new DefaultFileRenamePolicy());
+			mul = new MultipartRequest(req,uploadPath, 1024*10000,"utf-8",new DefaultFileRenamePolicy());
 		}catch(Exception e){
 			e.printStackTrace();
 			
